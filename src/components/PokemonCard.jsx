165d871f7078478f35097570016423dec4a262b2
ClaireVannier <clairevannierpro@gmail.com>
2023-10-09 21:32:14 +0200

    added propTypes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,34 @@
-function PokemonCard({pokemon}) {
+import PropTypes from "prop-types";
+
+
+function PokemonCard({ pokemon }) {
     console.log(pokemon);
     return (
         <div>
             <figure>
                 <figcaption>{pokemon.name}</figcaption>
-            {
-                pokemon.imgSrc ?
-                    <img width="200" src={pokemon.imgSrc} alt="photo de bulbasaur" />
-                    : <p>???</p>
-            }
+                {
+                    pokemon.imgSrc ?
+                        <img width="200" src={pokemon.imgSrc} alt="photo de bulbasaur" />
+                        : <p>???</p>
+                }
             </figure>
         </div>
-        );
+    );
+};
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
 };
 
 
+// // SayHello.propTypes = {
+//   wilder: PropTypes.shape({
+//     name: PropTypes.string.isRequired,
+//     languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   }).isRequired,
+// } 
 export default PokemonCard;
