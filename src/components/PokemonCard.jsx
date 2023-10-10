45a8a0eb0c14095ab17ad2a23c4830ec1117e8d9
ClaireVannier <clairevannierpro@gmail.com>
2023-10-10 10:41:56 +0200

    format document

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,29 @@
+            const pokemonList = [
+                {
+                    name: "bulbasaur",
+                    imgSrc:
+                        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+                },
+                {
+                    name: "mew",
+                },
+            ];
+
+
 function PokemonCard() {
-    let pokemon = pokemonList[0];
+    const pokemon = pokemonList[0];
     return (
         <div>
             <figure>
-                <figcaption>Bulbasaur</figcaption>
-            {
-                pokemon.imgSrc ?
-                    <img width="200" src={pokemon.imgSrc} alt="photo de bulbasaur" />
-                    : <p>???</p>
-            }
+                <figcaption>{pokemon.name}</figcaption>
+            {pokemon.imgSrc ?
+                    <img src={pokemon.imgSrc} alt="photo de bulbasaur" />
+                    : <p>???</p>}
             </figure>
         </div>
     );
 };
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
 
 export default PokemonCard;
